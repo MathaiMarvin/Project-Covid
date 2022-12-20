c03d3c1f4d576e5377b5a668229b1ef5d3ad04e6
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-12-20 20:56:15 +0300

    Displaying Country Statistics

--- js/index.js
+++ js/index.js
@@ -27,78 +27,78 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const searchDisplayContent = document.getElementById('search-card-country');
 
     //Creating the world data Element
-    function worldDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered ){
-        //Title of the card
-        titleCard.innerText = countryText;
-        //Body elements of the card
+    // function worldDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered ){
+    //     //Title of the card
+    //     titleCard.innerText = countryText;
+    //     //Body elements of the card
 
-        const worldCasesCovid = document.createElement('p')
-        worldCasesCovid.innerText = activeCases
+    //     const worldCasesCovid = document.createElement('p')
+    //     worldCasesCovid.innerText = activeCases
 
-        const worldLastUpdatedCovid = document.createElement('p')
-        worldLastUpdatedCovid.innerText = lastUpdated
+    //     const worldLastUpdatedCovid = document.createElement('p')
+    //     worldLastUpdatedCovid.innerText = lastUpdated
 
-        const worldNewCasesCovid = document.createElement('p')
-        worldNewCasesCovid.innerText = newCases
+    //     const worldNewCasesCovid = document.createElement('p')
+    //     worldNewCasesCovid.innerText = newCases
 
-        const worldnewDeathsCovid = document.createElement('p')
-        worldnewDeathsCovid.innerText = newDeaths
+    //     const worldnewDeathsCovid = document.createElement('p')
+    //     worldnewDeathsCovid.innerText = newDeaths
 
-        const worldTotalCasesCovid = document.createElement('p')
-        worldTotalCasesCovid.innerText = totalCases
+    //     const worldTotalCasesCovid = document.createElement('p')
+    //     worldTotalCasesCovid.innerText = totalCases
 
-        const worldTotalDeathsCovid = document.createElement('p')
-        worldTotalDeathsCovid.innerText = totalDeaths
+    //     const worldTotalDeathsCovid = document.createElement('p')
+    //     worldTotalDeathsCovid.innerText = totalDeaths
 
-        const worldTotalRecoveredCovid = document.createElement('p')
-        worldTotalRecoveredCovid.innerText = totalRecovered
+    //     const worldTotalRecoveredCovid = document.createElement('p')
+    //     worldTotalRecoveredCovid.innerText = totalRecovered
 
-        //Appending the card body elements
-        pWorldData.appendChild(worldCasesCovid)
-        pWorldData.appendChild(worldLastUpdatedCovid)
-        pWorldData.appendChild(worldNewCasesCovid)
-        pWorldData.appendChild(worldnewDeathsCovid)
-        pWorldData.appendChild(worldTotalCasesCovid)
-        pWorldData.appendChild(worldTotalDeathsCovid)
-        pWorldData.appendChild(worldTotalRecoveredCovid)
+    //     //Appending the card body elements
+    //     pWorldData.appendChild(worldCasesCovid)
+    //     pWorldData.appendChild(worldLastUpdatedCovid)
+    //     pWorldData.appendChild(worldNewCasesCovid)
+    //     pWorldData.appendChild(worldnewDeathsCovid)
+    //     pWorldData.appendChild(worldTotalCasesCovid)
+    //     pWorldData.appendChild(worldTotalDeathsCovid)
+    //     pWorldData.appendChild(worldTotalRecoveredCovid)
 
 
-        //appending the elements to the card
-        cardDiv.appendChild(pWorldData);
-        cardDiv.appendChild(titleCard);
+    //     //appending the elements to the card
+    //     cardDiv.appendChild(pWorldData);
+    //     cardDiv.appendChild(titleCard);
 
        
 
-        //Return the cardDisplayed
-        return cardDiv
+    //     //Return the cardDisplayed
+    //     return cardDiv
 
 
 
 
 
-    }
+    // }
 
     //Fetching the world Data
-    const loadWorldData =()=>{
-        fetch(worldUrl)
-        .then(res=>res.json())
-        .then((data)=>{
-            // console.log(data);
+    // const loadWorldData =()=>{
+    //     fetch(worldUrl)
+    //     .then(res=>res.json())
+    //     .then((data)=>{
+    //         // console.log(data);
 
-            const activeCases = ` Number of Active Cases: ${data["Active Cases_text"]}`
-            const countryText = `Region: ${data["Country_text"]}`
-            const lastUpdated = `Last Updated: ${data["Last Update"]}`
-            const newCases = `New Cases: ${data["New Cases_text"]}`
-            const newDeaths =  `New Deaths: ${data["New Deaths_text"]}`
-            const totalCases = `Total Cases: ${data["Total Cases_text"]}`
-            const totalDeaths  = `Total Deaths: ${data["Total Deaths_text"]}`
-            const totalRecovered = `Total Recovered: ${data["Total Recovered_text"]}`
+    //         const activeCases = ` Number of Active Cases: ${data["Active Cases_text"]}`
+    //         const countryText = `Region: ${data["Country_text"]}`
+    //         const lastUpdated = `Last Updated: ${data["Last Update"]}`
+    //         const newCases = `New Cases: ${data["New Cases_text"]}`
+    //         const newDeaths =  `New Deaths: ${data["New Deaths_text"]}`
+    //         const totalCases = `Total Cases: ${data["Total Cases_text"]}`
+    //         const totalDeaths  = `Total Deaths: ${data["Total Deaths_text"]}`
+    //         const totalRecovered = `Total Recovered: ${data["Total Recovered_text"]}`
 
-            const worldCreatedElement = worldDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered );
+    //         const worldCreatedElement = worldDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered );
             
-            cardDisplayed.appendChild(worldCreatedElement)
-        })
-    }
+    //         cardDisplayed.appendChild(worldCreatedElement)
+    //     })
+    // }
 
 
     //Creating the country data Element
@@ -179,19 +179,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     //Search form submit Listener
     searchForm.addEventListener('submit', (e) => {
+        searchDisplayContent.innerHTML = '';
 
         e.preventDefault();
+        
         const query = searchInput.value
+        
+
+
+
 
         searchCountry(query)
-        countryDiv.style.display = "none"
-        searchDisplayContent.style.display = "centre"
-        searchDisplayContent.removeAttribute('hidden')
-       
-
-
-
     })
+   
 
 
 
@@ -245,11 +245,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
     const searchCountry=(Country_text) =>{
+        // Clear the search results
+        searchDisplayContent.innerHTML = '';
+                
 
         fetch(`${searchCovidDataUrL}${Country_text}`)
             .then(response=>response.json())
             .then((data) => {
-                console.log(data);
+                //console.log(data);
+
+                
 
                 const activeCases = ` Number of Active Cases: ${data["Active Cases_text"]}`
                 const countryText = `Region: ${data["Country_text"]}`
@@ -271,8 +276,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 
 
+    //Click event for link
 
-    loadWorldData()
+
+
+
+ 
     countryCovidData()
     searchCountry()
 
