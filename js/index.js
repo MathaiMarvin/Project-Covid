7ff6aa3ce22bd42022cd6f0104396a4c8435a364
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-12-20 18:15:04 +0300

    Enabling Search

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // The URLS needed
-const covidDataUrL = 'https://covid-19.dataflowkit.com/v1/kenya';
+const searchCovidDataUrL = 'https://covid-19.dataflowkit.com/v1/';
 const worldUrl = 'https://covid-19.dataflowkit.com/v1/World';
+const covidDataUrL = 'https://covid-19.dataflowkit.com/v1/kenya';
 
 //DOM CONTENT LOADED EVENT LISTENER
 
@@ -15,6 +16,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const pCountryContent = document.getElementById('country-text-card');
     const countryDiv = document.getElementById('country-div');
     const cardCountryDisplay = document.getElementById('card-country');
+
+
+    //Search form
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('search')
+    const pSearch = document.getElementById('search-text-card')
+    const searchTitleCard = document.getElementById('search-title-card');
+    const divSearch = document.getElementById('search-div');
+    const searchDisplayContent = document.getElementById('search-card-country');
 
     //Creating the world data Element
     function worldDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered ){
@@ -160,12 +170,111 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
                 const countryCreatedElement = countryDataElement(activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered);
                 countryCreatedElement
+
             })
     }
 
 
+
+
+    //Search form submit Listener
+    searchForm.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+        const query = searchInput.value
+
+        searchCountry(query)
+        countryDiv.style.display = "none"
+        searchDisplayContent.style.display = "centre"
+        searchDisplayContent.removeAttribute('hidden')
+       
+
+
+
+    })
+
+
+
+    function searchDataElement (activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered) {
+
+        //Search Title
+        searchTitleCard.innerText = countryText
+
+        //Body Elements of the Card
+        const searchCountryCasesCovid = document.createElement('p')
+        searchCountryCasesCovid.innerText = activeCases
+
+        const searchCountryLastUpdatedCovid = document.createElement('p')
+        searchCountryLastUpdatedCovid.innerText = lastUpdated
+
+        const searchCountryNewCasesCovid = document.createElement('p')
+        searchCountryNewCasesCovid.innerText = newCases
+
+        const searchCountrynewDeathsCovid = document.createElement('p')
+        searchCountrynewDeathsCovid.innerText = newDeaths
+
+        const searchCountryTotalCasesCovid = document.createElement('p')
+        searchCountryTotalCasesCovid.innerText = totalCases
+
+        const searchCountryTotalDeathsCovid = document.createElement('p')
+        searchCountryTotalDeathsCovid.innerText = totalDeaths
+
+        const searchCountryTotalRecoveredCovid = document.createElement('p')
+        searchCountryTotalRecoveredCovid.innerText = totalRecovered
+
+
+        //Appending the elements to the cardbody
+        pSearch.appendChild(searchCountryCasesCovid)
+        pSearch.appendChild(searchCountryLastUpdatedCovid)
+        pSearch.appendChild(searchCountryNewCasesCovid)
+        pSearch.appendChild(searchCountrynewDeathsCovid)
+        pSearch.appendChild(searchCountryTotalCasesCovid)
+        pSearch.appendChild(searchCountryTotalDeathsCovid)
+        pSearch.appendChild(searchCountryTotalRecoveredCovid)
+
+
+        //Appending elements to the card
+        divSearch.appendChild(pSearch)
+        divSearch.appendChild(searchTitleCard)
+
+        //Returning the countries Displayed
+        searchDisplayContent.appendChild(divSearch)
+
+    }
+
+
+
+    const searchCountry=(Country_text) =>{
+
+        fetch(`${searchCovidDataUrL}${Country_text}`)
+            .then(response=>response.json())
+            .then((data) => {
+                console.log(data);
+
+                const activeCases = ` Number of Active Cases: ${data["Active Cases_text"]}`
+                const countryText = `Region: ${data["Country_text"]}`
+                const lastUpdated = `Last Updated: ${data["Last Update"]}`
+                const newCases = `New Cases: ${data["New Cases_text"]}`
+                const newDeaths =  `New Deaths: ${data["New Deaths_text"]}`
+                const totalCases = `Total Cases: ${data["Total Cases_text"]}`
+                const totalDeaths  = `Total Deaths: ${data["Total Deaths_text"]}`
+                const totalRecovered = `Total Recovered: ${data["Total Recovered_text"]}`
+
+
+                const searchCreatedElement = searchDataElement(activeCases, countryText, lastUpdated, newCases, newDeaths, totalCases, totalDeaths, totalRecovered);
+
+                searchCreatedElement
+
+            })
+
+
+    }
+
+
+
     loadWorldData()
     countryCovidData()
+    searchCountry()
 
 
 
